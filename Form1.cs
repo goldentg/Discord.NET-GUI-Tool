98e3f8be0307e16aec0dbfa6c22d722165a821d4
GoldenCodex <evan.norman@me.com>
2021-06-06 00:18:01 -0600

    commenting

--- Form1.cs
+++ Form1.cs
@@ -16,8 +16,10 @@ namespace Discord.NET_GUI_Tool
 
         private async void connect_b_Click(object sender, EventArgs e)
         {
+            //Create a new socket client
             Client = new DiscordSocketClient(new DiscordSocketConfig
             {
+                //API settings
                 LogLevel = LogSeverity.Verbose,
                 AlwaysDownloadUsers = true,
                 MessageCacheSize = 200
@@ -27,6 +29,7 @@ namespace Discord.NET_GUI_Tool
 
             try
             {
+                //Login to API with token provided in token_tb field
                 await Client.LoginAsync(TokenType.Bot, token_tb.Text);
                 await Client.StartAsync();
             }
@@ -42,6 +45,7 @@ namespace Discord.NET_GUI_Tool
 
         private Task Client_Log(LogMessage arg)
         {
+            //Display console output/logging to console_output_rtb
             Invoke((Action) delegate { console_output_rtb.AppendText(arg + "\n"); });
             return null;
         }
